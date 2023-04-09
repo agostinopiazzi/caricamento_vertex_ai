bb687ef9b517da0ca28a0860d21d610cec7de9f2
agostino.piazzi <agostino.piazzi@hotmaili.it>
2023-04-09 14:43:31 +0200

    update

--- automatismo.py
+++ automatismo.py
@@ -1,24 +1,39 @@
 
-REGION = "europe-west4a"
-PROJECT_ID = "bper-ai-ew-test-380011"
-ZONE="europe-west4-a"
-BUCKET_NAME = "gs://bper-ai-ew-test-pipeline-integration"
-PIPELINE_ROOT = f"{BUCKET_NAME}/pipeline_root/"
-DATAPROC_CLUSTER="bper-ai-dataproc-cluster"
 
-base_image="python:3.9",
-output_component_file="load_data_from_bucket_component.yaml",
-packages_to_install=["google-cloud-dataproc","google-cloud-storage"]
-
-region="europe-west4"
-cluster_name="bper-ai-dataproc-cluster"
+project_id = "bper-ai-ew-test-380011"
+region = "europe-west4"
+cluster_name = "bper-ai-dataproc-cluster"
 uri_custom_image = "europe-west4-docker.pkg.dev/bper-ai-ew-test-380011/nomerepository2/nometag3:latest"
-
+properties_file_path = "tbd"
 main_python_file_uri = "gs://bper-ai-ew-test-pipeline-integration/load_data_job.py"
+extra_py_file_path ="tbd"
+input_files_paths = ["inp1", "inp2"]
+output_file_path = "out"
+
+base_image = "python:3.9"
+output_component_file = "load_data_from_bucket.yaml"
+packages_to_install = "[\"google-cloud-storage\", \"google-cloud-dataproc\"]"
 
 
 with open("template_job.txt", 'r', encoding='utf8') as source:
     lines = source.read()
-    lines = lines.replace('@region@','\'europe-west4\'')
-    with open("example.py", 'w') as target:
+    lines = lines.replace('@project_id@', project_id)
+    lines = lines.replace('@region@', region)
+    lines = lines.replace('@cluster_name@', cluster_name)
+    lines = lines.replace('@uri_custom_image@', uri_custom_image)
+    lines = lines.replace('@properties_file_path@', properties_file_path)
+    lines = lines.replace('@main_python_file_uri@', main_python_file_uri)
+    lines = lines.replace('@extra_py_file_path@', extra_py_file_path)
+    lines = lines.replace('@packages_to_install@', packages_to_install)
+    lines = lines.replace('@base_image@', base_image)
+    lines = lines.replace('@output_component_file@', output_component_file)
+
+    input_files_paths_string = "["
+    for path in input_files_paths:
+        input_files_paths_string += f"\"{path}\", "
+    input_files_paths_string += "]"
+
+    lines = lines.replace('@input_files_paths@', input_files_paths_string)
+    lines = lines.replace('@output_file_path@', output_file_path)
+    with open("example.py", 'w', encoding='utf8') as target:
         target.write(lines)
--- example.py
+++ example.py
@@ -1,49 +1,30 @@
-
-# !pip3 install google-cloud-aiplatform==1.0.0 --upgrade
-# !pip3 install kfp google-cloud-pipeline-components==1.0.8
-
-REGION='europe-west4'
-PROJECT_ID=@project_id@
-ZONE="europe-west4-a"
-BUCKET_NAME = "gs://bper-ai-ew-test-pipeline-integration"
-PIPELINE_ROOT = f"{BUCKET_NAME}/pipeline_root/"
-DATAPROC_CLUSTER="bper-ai-dataproc-cluster"
-
-
-project_id=PROJECT_ID
-region=REGION
-cluster_name=DATAPROC_CLUSTER
-
-from typing import NamedTuple
-import kfp
-from kfp import dsl
-from kfp.v2 import compiler
-from kfp.v2.dsl import (Artifact, Dataset, Input, InputPath, Model, Output,
-                        OutputPath, ClassificationMetrics, Metrics, component)
-from kfp.v2.google.client import AIPlatformClient
-from google_cloud_pipeline_components.v1.custom_job import create_custom_training_job_from_component
+from kfp.v2.dsl import component
 
 
 @component(
-    base_image="python:3.9", 
-    output_component_file="load_data_from_bucket_component.yaml", 
-    packages_to_install=["google-cloud-dataproc","google-cloud-storage"]
+    base_image="python:3.9",
+    output_component_file="load_data_from_bucket.yaml",
+    packages_to_install=["google-cloud-storage", "google-cloud-dataproc"]
 )
 def load_data_from_bucket():
-    
-    import re
 
+    import re
     import google.cloud.dataproc_v1 as dataproc
     import google.cloud.storage as storage
-    
-    import json
-    
-    project_id="bper-ai-ew-test-380011"
-    region="europe-west4"
-    cluster_name="bper-ai-dataproc-cluster"
+
+    project_id = "bper-ai-ew-test-380011"
+    region = "europe-west4"
+    cluster_name = "bper-ai-dataproc-cluster"
     uri_custom_image = "europe-west4-docker.pkg.dev/bper-ai-ew-test-380011/nomerepository2/nometag3:latest"
-    
-    
+    properties_file_path = "tbd"
+    main_python_file_uri = "gs://bper-ai-ew-test-pipeline-integration/load_data_job.py"
+    extra_py_file_path = "tbd"
+    input_files_paths = ["inp1", "inp2", ]
+    output_file_path = "out"
+
+    all_in_out_paths = input_files_paths + [output_file_path]
+    all_in_out_paths.sort(key=lambda x: x.split('/')[-1])
+
     # Create the job client.
     job_client = dataproc.JobControllerClient(
         client_options={"api_endpoint": "{}-dataproc.googleapis.com:443".format(region)}
@@ -56,10 +37,13 @@ def load_data_from_bucket():
             'cluster_name': cluster_name
         },
         'pyspark_job': {
-            'main_python_file_uri': "gs://bper-ai-ew-test-pipeline-integration/load_data_job.py",
+            'main_python_file_uri': main_python_file_uri,
             'properties':{
                 "spark.kubernetes.container.image": uri_custom_image
             },
+            'properties-file': properties_file_path,
+            'py-files': extra_py_file_path
+            'args': all_in_out_paths
         }
     }
 
@@ -80,34 +64,5 @@ def load_data_from_bucket():
     )
 
     print(f"Job finished successfully: {output}")
-
-
-# +
-#cmp1 = kfp.components.load_component_from_file("dataproc-cluster-component.yaml")
-
-# +
-@dsl.pipeline(
-    name="prediction-bper",
-    
-)
-def test_bper():
-    task1 = load_data_from_bucket()
-
-
-# -
-
-compiler.Compiler().compile(
-    pipeline_func=test_bper, package_path="bper_test_dataproc_job.json"
-)
-
-api_client = AIPlatformClient(
-    project_id=PROJECT_ID,
-    region=REGION,
-)
-response = api_client.create_run_from_job_spec(
-    job_spec_path="bper_test_dataproc_job.json",
-    enable_caching=False,
-    pipeline_root=PIPELINE_ROOT  # this argument is necessary if you did not specify PIPELINE_ROOT as part of the pipeline definition.
-)
 
 
