26eab40372375b41896ef70c5a4ad72a6de39444
agostino.piazzi <agostino.piazzi@hotmaili.it>
2023-04-11 16:14:34 +0200

    wip

--- helper_module.py
+++ helper_module.py
@@ -2,11 +2,12 @@ import sys
 from typing import Optional
 
 
-def get_input(sc, input_name:str, read_kwargs:Optional[dict] = None):
-    list = sys.argv[1:-1]
-    list = list.sort(key=lambda x: x.split('/')[-1])
-    list = [x.split('/')[-1] for x in list]
-    index = list.index(input_name)
+def get_input(sc, input_name: str, read_kwargs:Optional[dict] = None):
+
+    input_list = sys.argv[1:-1]
+    input_list_names = [x.split('/')[-1] for x in input_list]
+
+    index = input_list_names.index(input_name)
     if read_kwargs:
         sdf = sc.read_parquet(sys.argv[index], **read_kwargs)
     else:
@@ -14,7 +15,7 @@ def get_input(sc, input_name:str, read_kwargs:Optional[dict] = None):
     return sdf
 
 
-def set_output(sdf, output_name:str, write_kwargs:Optional[dict] = None):
+def set_output(sdf, write_kwargs: Optional[dict] = None):
     if write_kwargs:
         sdf.write_parquet(sys.argv[-1], **write_kwargs)
     else:
